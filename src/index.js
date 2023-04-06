40b1e5d4a642e03012a622018b71427541a0b056
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-06 22:45:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import './stylesheet/index.css';
+import './stylesheet/vars.css';
+import './stylesheet/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
