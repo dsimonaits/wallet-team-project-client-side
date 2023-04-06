7b9e8edf3fd7be5ef1b4eb7da0304a37d1f6deb5
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-06 23:11:22 +0300

    added Test Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Test from './Test';
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Test />} />
       </Routes>
     </>
   );
