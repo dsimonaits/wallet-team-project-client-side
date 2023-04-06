9079c7a9d05b265bd2530d9d84bd2f0a68f75e4e
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-06 23:03:11 +0300

    Added browserRouter for Routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
 import './stylesheet/index.css';
 import './stylesheet/vars.css';
 import './stylesheet/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter basename="wallet-team-project-client-side"></BrowserRouter>
     <App />
   </React.StrictMode>
 );
