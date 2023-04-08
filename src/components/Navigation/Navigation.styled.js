bf80eb936f184b3ef112ba296977427c246dbfa7
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-08 17:32:26 +0300

    Create Navigation.styled.js

---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Nav = styled.nav`
+  padding-top: 15px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  list-style: none;
+
+  @media screen and (max-width: 767.9px) {
+    max-width: 186px;
+
+  };
+
+  @media screen and (min-width: 768px) {
+    flex-direction: column;
+
+  }
+`;
+
+export const NavItem = styled.li`
+
+  @media screen and (min-width: 768px) {
+  
+    &:not(:first-child) {
+      margin-top: 21px;
+    }
+
+}
+`
+
+
+export const LinkName = styled.span`
+font-size: 18px;
+font-weight: 400;
+font-family: var( --secondary-font-family);
+
+
+
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+    clip-path: inset(100%);
+    clip: rect(0 0 0 0);
+    overflow: hidden;
+  }
+
+  @media screen and (min-width: 768px) {
+  
+    margin-left: 23px;
+  
+  }
+`;
+
+export const NavLinkStyled = styled(NavLink)`
+color: var( --primary-text-color);
+text-decoration: none;
+  @media screen and (min-width: 768px) {
+  
+  display:flex;
+  align-items:center;
+
+}
+
+  &:hover ,
+  &:focus {
+    filter: drop-shadow( 0px 3px 10px #4a56e280);
+
+  }
+
+  &.active svg {
+    filter: drop-shadow( 0px 3px 10px #4a56e280);
+  }
+  &.active span {
+    font-weight: 700;
+  }
+`;
