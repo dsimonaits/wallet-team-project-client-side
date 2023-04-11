fa44110fa874bfad59885359b74d94b5011ca620
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2023-04-11 11:16:13 +0300

    Modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,33 @@
+import Media from 'react-media'
+import { mediaQueries } from '../../utils/media'
+import {ModalWrapper, ModalContent, CloseBtnBox, CloseButton, CloseIcon} from './Modal.styled'
+import sprite from  '../../images/sprite.svg'
+
+
+export default function Modal({children}) {
+
+  return (
+    <>
+    <ModalWrapper>
+      <ModalContent>
+      <Media queries={mediaQueries}>
+              {(matches) =>
+                (matches.tablet || matches.desktop) && (
+                   <CloseBtnBox>
+        <CloseButton>
+          <CloseIcon width="24" height="24">
+            <use href={`${sprite}#icon-close`} />
+          </CloseIcon>
+        </CloseButton>
+      </CloseBtnBox>
+                )
+              }
+            </Media>
+      {children}
+      </ModalContent>
+    </ModalWrapper>
+    </>
+  );
+   
+}
+
