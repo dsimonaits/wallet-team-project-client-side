7990263b84931294c5c0e83da1866a3f656da35a
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-11 22:20:52 +0300

    basename back

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate persistor={persistor}> */}
-      <BrowserRouter>
+      <BrowserRouter basename="wallet-team-project-client-side">
         <App />
       </BrowserRouter>
       {/* </PersistGate> */}
