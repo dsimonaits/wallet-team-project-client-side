8bfda7c2f9d84424bf7837ee3debcde749fdf61a
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-11 22:05:34 +0300

    Redux store + GlobalSlice and selectors

---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { globalReducer } from "./global/globalSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const authPersistConfig = {
+  key: "session",
+  storage,
+  whitelist: ["token"],
+};
+
+export const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    // session: persistReducer(authPersistConfig, "sessionReducer"),
+    // finance: "financeReducer",
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// export const persistor = persistStore(store);
