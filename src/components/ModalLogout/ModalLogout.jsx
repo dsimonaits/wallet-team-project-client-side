2d2a13b9ea6b951282d52db63e237ffd1b180844
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-13 21:53:19 +0300

    mediaQueries default export and all files fixed to default import

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,31 +1,30 @@
-import { mediaQueries } from '../../utils/media'
-import Media from 'react-media'
-import {CancelButton, ExitButton, LogoContainer, ModalContainer, Text} from './ModalLogout.styled'
-import Modal from 'components/Modal/Modal'
-import Logo from 'components/Logo/Logo'
-
+import mediaQueries from '../../utils/media';
+import Media from 'react-media';
+import {
+  CancelButton,
+  ExitButton,
+  LogoContainer,
+  ModalContainer,
+  Text,
+} from './ModalLogout.styled';
+import Modal from 'components/Modal/Modal';
+import Logo from 'components/Logo/Logo';
 
 export default function ModalLogout() {
-
-    return (
-      <>
-        <Modal>
+  return (
+    <>
+      <Modal>
         <ModalContainer>
           <LogoContainer>
-          <Media queries={mediaQueries}>
-              {(matches) =>
-                (matches.mobile) && (
-               <Logo />
-                )
-              }
-            </Media>   
-          
+            <Media queries={mediaQueries}>
+              {matches => matches.mobile && <Logo />}
+            </Media>
           </LogoContainer>
-        <Text>Are you sure you want to exit?</Text>
-        <ExitButton>Exit</ExitButton>
-        <CancelButton>Cancel</CancelButton>
-      </ModalContainer>
+          <Text>Are you sure you want to exit?</Text>
+          <ExitButton>Exit</ExitButton>
+          <CancelButton>Cancel</CancelButton>
+        </ModalContainer>
       </Modal>
     </>
-    )
+  );
 }
--- src/utils/media.js
+++ src/utils/media.js
@@ -1,7 +1,8 @@
-export const mediaQueries = {
-    response: '(max-width: 319px)',
-    mobile: '(min-width: 320px) and (max-width: 767px)',
-    tablet: '(min-width: 768px) and (max-width: 1279px)',
-    desktop: '(min-width: 1280px)',
-  }
+const mediaQueries = {
+  response: '(max-width: 319px)',
+  mobile: '(min-width: 320px) and (max-width: 767px)',
+  tablet: '(min-width: 768px) and (max-width: 1279px)',
+  desktop: '(min-width: 1280px)',
+};
 
+export default mediaQueries;
