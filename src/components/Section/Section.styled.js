bf3d825addb8e7491b9c7865ec54a7cba58e3762
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-14 03:22:26 +0300

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -4,7 +4,7 @@ export const SectionStyled = styled.section`
   background-color: var(--background-color);
   backdrop-filter: blur(25px);
   padding-top: 12px;
-  height: 100%;
+  min-height: 100vh;
 
   @media screen and (min-width: 768px) {
     padding-top: 32px;
