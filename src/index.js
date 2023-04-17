5e7bfcc84aacb2b0f64a83207917a56f751e43f9
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-17 23:58:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
-// import { persistor } from './redux/store';
+import { persistor } from './redux/store';
 import './stylesheet/index.css';
 import './stylesheet/vars.css';
 import './stylesheet/fonts.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    {/* <PersistGate persistor={persistor}> */}
-    <BrowserRouter basename="wallet-team-project-client-side">
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="wallet-team-project-client-side">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
