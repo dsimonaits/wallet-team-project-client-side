aa06173f3737911bb36d5db4d083c16f99674d22
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-18 21:42:48 +0300

    Basename deleted, maybe issue of no page found

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="wallet-team-project-client-side">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
