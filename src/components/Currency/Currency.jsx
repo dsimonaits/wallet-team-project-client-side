c2e416c02d23eea1cb96dc163d7af67b5befc4fb
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-29 23:59:23 +0300

    Update Currency.jsx

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -2,14 +2,9 @@ import currenciesApi from '../../services/api/currenciesApi';
 import currenCiesList from './currenciesList.json';
 import { useState, useEffect } from 'react';
 import { Table, CurrencyWrapper } from './Currency.styled';
-import { selectIsLoading } from 'redux/finance/financeSelectors';
-import Spinner from 'components/Spinner/Spinner';
-import { useSelector } from 'react-redux';
 
 const Currency = () => {
   const [currencyCourse, setCurrencyCourse] = useState(null);
-
-  const fetching = useSelector(selectIsLoading);
 
   useEffect(() => {
     const fetchCurrencies = async () => {
@@ -28,42 +23,38 @@ const Currency = () => {
 
   return (
     <CurrencyWrapper>
-      {fetching ? (
-        <Spinner />
-      ) : (
-        <Table>
-          <thead>
-            <tr>
-              <th>Currency</th>
-              <th>Purchase</th>
-              <th>Sale</th>
-            </tr>
-          </thead>
+      <Table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
 
-          <tbody>
-            {currencyCourse &&
-              currencyCourse.map((item, index) => {
-                const currencyCode = item.currencyCodeA;
-                const currencyCodeToString = currencyCode.toString();
-                const currencyName = currenCiesList[currencyCodeToString];
+        <tbody>
+          {currencyCourse &&
+            currencyCourse.map((item, index) => {
+              const currencyCode = item.currencyCodeA;
+              const currencyCodeToString = currencyCode.toString();
+              const currencyName = currenCiesList[currencyCodeToString];
 
-                if (
-                  currenCiesList[currencyCodeToString] &&
-                  !(item.currencyCodeA === 978 && item.currencyCodeB === 840)
-                ) {
-                  return (
-                    <tr key={index}>
-                      <td>{currencyName}</td>
-                      <td>{item.rateBuy.toFixed(2)}</td>
-                      <td>{item.rateSell.toFixed(2)}</td>
-                    </tr>
-                  );
-                }
-                return null;
-              })}
-          </tbody>
-        </Table>
-      )}
+              if (
+                currenCiesList[currencyCodeToString] &&
+                !(item.currencyCodeA === 978 && item.currencyCodeB === 840)
+              ) {
+                return (
+                  <tr key={index}>
+                    <td>{currencyName}</td>
+                    <td>{item.rateBuy.toFixed(2)}</td>
+                    <td>{item.rateSell.toFixed(2)}</td>
+                  </tr>
+                );
+              }
+              return null;
+            })}
+        </tbody>
+      </Table>
     </CurrencyWrapper>
   );
 };
