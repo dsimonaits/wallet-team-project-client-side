c874de5edb94d0631b77f74c5b9fc9e88465a896
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-30 00:03:13 +0300

    Update Spinner.jsx

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsRefreshing } from 'redux/session/sessionSelectors';
+import { selectIsLoading } from 'redux/finance/financeSelectors';
 import { ThreeDots } from 'react-loader-spinner';
 import { SpinnerContainer } from './Spinner.styled';
 
 const Spinner = ({ position }) => {
   const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoading = useSelector(selectIsLoading);
 
-  if (!isRefreshing) {
+  if (!isRefreshing && !isLoading) {
     return null;
   }
 
