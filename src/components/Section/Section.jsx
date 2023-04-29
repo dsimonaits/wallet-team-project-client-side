3920ae172fc0f8e09729850b64f88d0146506987
Deniss Simonaits <dsimonaits@gmail.com>
2023-04-29 20:12:27 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
-import { toggleIsLoading } from 'redux/global/globalSlice';
 import { SectionStyled } from './Section.styled';
-import { useDispatch } from 'react-redux';
 
 const Section = ({ children }) => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(toggleIsLoading());
-  }, [dispatch]);
   return <SectionStyled id="blur">{children}</SectionStyled>;
 };
 
