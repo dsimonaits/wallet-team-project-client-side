5da1696f47ef6fa51adc4fa30105119118fe5843
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-18 01:25:22 +0300

    Update RegistrationPage.jsx

--- src/pages/auth/RegistrationPage.jsx
+++ src/pages/auth/RegistrationPage.jsx
@@ -1,30 +1,37 @@
-import RegisterForm from '../../components/RegForm/RegForm'
+import RegisterForm from '../../components/RegForm/RegForm';
 import Media from 'react-media';
 import mediaQueries from '../../utils/media';
 import {
-    Section,
-    PageContainer,
-    ImgContainer,
-    ImgWrapperReg,
-    Title,
-    FormWrapper,
+  Section,
+  PageContainer,
+  ImgContainer,
+  ImgWrapperReg,
+  Title,
+  FormWrapper,
 } from './AuthPages.styled';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const RegistrationPage = () => {
   return (
     <Section>
       <PageContainer>
-         <Media queries={mediaQueries}>
-              {matches => (matches.tablet || matches.desktop) &&  <ImgContainer>
-            <ImgWrapperReg />
-          <Title>Finance App</Title>
-        </ImgContainer>}
-            </Media>     
-              <FormWrapper>
-                 <RegisterForm />   
-              </FormWrapper>
-          </PageContainer>        
-     </Section>
+        <Media queries={mediaQueries}>
+          {matches =>
+            (matches.tablet || matches.desktop) && (
+              <ImgContainer>
+                <ImgWrapperReg />
+                <Title>Finance App</Title>
+              </ImgContainer>
+            )
+          }
+        </Media>
+        <FormWrapper>
+          <RegisterForm />
+        </FormWrapper>
+      </PageContainer>
+      <ToastContainer />
+    </Section>
   );
 };
 export default RegistrationPage;
