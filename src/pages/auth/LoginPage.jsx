886e6dd0dc214afa6c1d5c126c5774142e84ae1e
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-18 01:25:20 +0300

    Update LoginPage.jsx

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -2,29 +2,36 @@ import LoginForm from 'components/LoginForm/LoginForm';
 import Media from 'react-media';
 import mediaQueries from '../../utils/media';
 import {
-    Section,
-    PageContainer,
-    ImgContainer,
-    ImgWrapperLogin,
-    Title,
-    FormWrapper,
+  Section,
+  PageContainer,
+  ImgContainer,
+  ImgWrapperLogin,
+  Title,
+  FormWrapper,
 } from './AuthPages.styled';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginPage = () => {
   return (
     <Section>
       <PageContainer>
-         <Media queries={mediaQueries}>
-              {matches => (matches.tablet || matches.desktop) &&  <ImgContainer>
-            <ImgWrapperLogin />
-          <Title>Finance App</Title>
-        </ImgContainer>}
-            </Media>       
-              <FormWrapper>
-                <LoginForm />   
-              </FormWrapper>
-          </PageContainer>        
-     </Section>
+        <Media queries={mediaQueries}>
+          {matches =>
+            (matches.tablet || matches.desktop) && (
+              <ImgContainer>
+                <ImgWrapperLogin />
+                <Title>Finance App</Title>
+              </ImgContainer>
+            )
+          }
+        </Media>
+        <FormWrapper>
+          <LoginForm />
+        </FormWrapper>
+      </PageContainer>
+      <ToastContainer />
+    </Section>
   );
 };
 export default LoginPage;
