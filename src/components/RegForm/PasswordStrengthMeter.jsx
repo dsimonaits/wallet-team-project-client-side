90f7cb74eb9a8799066c29635f8dc21a1bb11c8c
Deniss Simonaits <dsimonaits@gmail.com>
2023-09-18 13:12:12 +0300

    Update PasswordStrengthMeter.jsx

--- src/components/RegForm/PasswordStrengthMeter.jsx
+++ src/components/RegForm/PasswordStrengthMeter.jsx
@@ -3,39 +3,38 @@ import zxcvbn from 'zxcvbn';
 
 const PasswordStrengthMeter = ({ password }) => {
   const testResult = zxcvbn(password);
-  const num = testResult.score * 100/4;
+  const num = (testResult.score * 100) / 4;
 
-  
   const funcProgressColor = () => {
-    switch(testResult.score) {
+    switch (testResult.score) {
       case 0:
         return '#828282';
       case 1:
-        return 'var(--greenblue)';
+        return '#EA1111';
       case 2:
-        return 'var(--greenblue)';
+        return '#FFAD00';
       case 3:
-        return 'var(--greenblue)';
+        return '#9bc158';
       case 4:
         return 'var(--greenblue)';
       default:
         return 'none';
     }
-  }
+  };
 
   const changePasswordColor = () => ({
     width: `${num}%`,
     background: funcProgressColor(),
-    height: '5px'
-  })
+    height: '5px',
+  });
 
   return (
     <>
       <div className="progress" style={{ height: '5px' }}>
         <div className="progress-bar" style={changePasswordColor()}></div>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default PasswordStrengthMeter;
