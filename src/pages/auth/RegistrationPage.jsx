01f9fcd6d8f3ba0789bcd605d96b169e5f2a4c9d
Deniss Simonaits <dsimonaits@gmail.com>
2023-10-02 00:01:25 +0300

    Update RegistrationPage.jsx

--- src/pages/auth/RegistrationPage.jsx
+++ src/pages/auth/RegistrationPage.jsx
@@ -2,11 +2,11 @@ import RegisterForm from 'components/Forms/RegForm/RegForm';
 import { ImgWrapperReg } from 'components/Layouts/AuthLayout/AuthLayout.styled';
 import AuthLayout from 'components/Layouts/AuthLayout/AuthLayout';
 
-const LoginPage = () => {
+const RegistrationPage = () => {
   return (
     <AuthLayout ImgWrapper={ImgWrapperReg}>
       <RegisterForm />
     </AuthLayout>
   );
 };
-export default LoginPage;
+export default RegistrationPage;
